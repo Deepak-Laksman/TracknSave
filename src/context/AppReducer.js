da169d1f3721adc63ee11a405b8bc58a6ab3ab37
Deepak-Laksman <deepaklaksmanj@gmail.com>
2023-04-12 10:12:38 +0530

    debugging

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default AppReducer =  (state, action) => {
+export const AppReducer =  (state, action) => {
     switch (action.type) {
         case "ADD_TRANSACTION":
             return {...state, transactions: [ action.payload, ...state.transactions]}
